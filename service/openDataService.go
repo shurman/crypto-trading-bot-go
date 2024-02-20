5ce63513a9940a0c6a572800d6ebe0526170538b
shurman <shurman@PC>
2024-02-21 01:03:19 +0800

    feat: Update exchange pair list

--- service/openDataService.go
+++ service/openDataService.go
@@ -6,6 +6,21 @@ import (
 	"github.com/adshao/go-binance/v2/futures"
 )
 
+func GetAllExchangeSymbol() []string {
+	result, err := FuturesClient.NewExchangeInfoService().Do(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+
+	var symbolSlice []string
+	for _, v := range result.Symbols {
+		symbolSlice = append(symbolSlice, v.Pair)
+	}
+
+	return symbolSlice
+}
+
 func GetHistoryOpenInterest(symbol string, period string, endTime int64) *futures.OpenInterestStatistic {
 
 	resultSlice, err := FuturesClient.NewOpenInterestStatisticsService().
